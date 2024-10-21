398986e317fa27dd95b579b1efa7f0b506507aaa
Grayyy1219 <lance.musngi@gmail.com>
2024-10-21 15:46:36 +0800

    new

--- home.php
+++ home.php
@@ -1,47 +1,57 @@
-<?php include 'db_connect.php' ?>
+<?php include 'db_connect.php'; ?>
 <style>
-   span.float-right.summary_icon {
-    font-size: 3rem;
-    position: absolute;
-    right: 1rem;
-    top: 0;
-}
-.imgs{
-		margin: .5em;
-		max-width: calc(100%);
-		max-height: calc(100%);
-	}
-	.imgs img{
-		max-width: calc(100%);
-		max-height: calc(100%);
-		cursor: pointer;
-	}
-	#imagesCarousel,#imagesCarousel .carousel-inner,#imagesCarousel .carousel-item{
-		height: 60vh !important;background: black;
-	}
-	#imagesCarousel .carousel-item.active{
-		display: flex !important;
-	}
-	#imagesCarousel .carousel-item-next{
-		display: flex !important;
-	}
-	#imagesCarousel .carousel-item img{
-		margin: auto;
-	}
-	#imagesCarousel img{
-		width: auto!important;
-		height: auto!important;
-		max-height: calc(100%)!important;
-		max-width: calc(100%)!important;
-	}
+    span.float-right.summary_icon {
+        font-size: 3rem;
+        position: absolute;
+        right: 1rem;
+        top: 0;
+    }
+
+    .imgs {
+        margin: .5em;
+        max-width: calc(100%);
+        max-height: calc(100%);
+    }
+
+    .imgs img {
+        max-width: calc(100%);
+        max-height: calc(100%);
+        cursor: pointer;
+    }
+
+    #imagesCarousel,
+    #imagesCarousel .carousel-inner,
+    #imagesCarousel .carousel-item {
+        height: 60vh !important;
+        background: black;
+    }
+
+    #imagesCarousel .carousel-item.active {
+        display: flex !important;
+    }
+
+    #imagesCarousel .carousel-item-next {
+        display: flex !important;
+    }
+
+    #imagesCarousel .carousel-item img {
+        margin: auto;
+    }
+
+    #imagesCarousel img {
+        width: auto !important;
+        height: auto !important;
+        max-height: calc(100%) !important;
+        max-width: calc(100%) !important;
+    }
 </style>
 
 <div class="containe-fluid">
-	<div class="row mt-3 ml-3 mr-3">
+    <div class="row mt-3 ml-3 mr-3">
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-body">
-                    <?php echo "Welcome back ". $_SESSION['login_name']."!"  ?>
+                    <?php echo "Welcome back " . $_SESSION['login_name'] . "!"  ?>
                     <hr>
                     <div class="row">
                         <div class="col-md-4 mb-3">
@@ -50,8 +60,8 @@
                                     <div class="card-body text-white">
                                         <span class="float-right summary_icon"> <i class="fa fa-home "></i></span>
                                         <h4><b>
-                                            <?php echo $conn->query("SELECT * FROM houses")->num_rows ?>
-                                        </b></h4>
+                                                <?php echo $conn->query("SELECT * FROM houses")->num_rows ?>
+                                            </b></h4>
                                         <p><b>Total Houses</b></p>
                                     </div>
                                 </div>
@@ -70,8 +80,8 @@
                                     <div class="card-body text-white">
                                         <span class="float-right summary_icon"> <i class="fa fa-user-friends "></i></span>
                                         <h4><b>
-                                            <?php echo $conn->query("SELECT * FROM tenants where status = 1 ")->num_rows ?>
-                                        </b></h4>
+                                                <?php echo $conn->query("SELECT * FROM tenants where status = 1 ")->num_rows ?>
+                                            </b></h4>
                                         <p><b>Total Tenants</b></p>
                                     </div>
                                 </div>
@@ -90,11 +100,11 @@
                                     <div class="card-body text-white">
                                         <span class="float-right summary_icon"> <i class="fa fa-file-invoice "></i></span>
                                         <h4><b>
-                                            <?php 
-                                             $payment = $conn->query("SELECT sum(amount) as paid FROM payments where date(date_created) = '".date('Y-m-d')."' "); 
-                                             echo $payment->num_rows > 0 ? number_format($payment->fetch_array()['paid'],2) : 0;
-                                             ?>
-                                        </b></h4>
+                                                <?php
+                                                $payment = $conn->query("SELECT sum(amount) as paid FROM payments where date(date_created) = '" . date('Y-m-d') . "' ");
+                                                echo $payment->num_rows > 0 ? number_format($payment->fetch_array()['paid'], 2) : 0;
+                                                ?>
+                                            </b></h4>
                                         <p><b>Payments This Month</b></p>
                                     </div>
                                 </div>
@@ -113,86 +123,144 @@
                                     <div class="card-body text-white">
                                         <span class="float-right summary_icon"> <i class="fa fa-file-invoice "></i></span>
                                         <h4><b>
-                                            <?php 
-                                             $payment = $conn->query("SELECT sum(amount) as paid FROM payments where date(date_created) = '".date('Y-m-d')."' "); 
-                                             echo $payment->num_rows > 0 ? number_format($payment->fetch_array()['paid'],2) : 0;
-                                             ?>
-                                        </b></h4>
-                                        <p><b>Next Month Payment Prediction</b></p>
+                                                <?php
+
+                                                $currentMonth = date("Y-m-01 00:00:00");
+                                                $sixMonthsAgo = date("Y-m-d H:i:s", strtotime("-6 months", strtotime($currentMonth)));
+
+                                                // Query to get payment data from the last 6 months
+                                                $sql = "SELECT DATE_FORMAT(date_created, '%Y-%m') AS payment_month, SUM(amount) AS total_income
+        FROM payments
+        WHERE date_created >= '$sixMonthsAgo'
+        GROUP BY payment_month
+        ORDER BY payment_month ASC";
+
+                                                $result = $conn->query($sql);
+
+                                                $monthsData = [];
+                                                $previousIncome = null;
+                                                $growthRates = [];
+                                                $totalGrowth = 0;
+                                                $monthsCount = 0;
+
+                                                if ($result->num_rows > 0) {
+                                                    while ($row = $result->fetch_assoc()) {
+                                                        $monthsData[] = $row;
+
+                                                        // Calculate growth rate between consecutive months
+                                                        if ($previousIncome !== null) {
+                                                            $growthRate = ($row['total_income'] - $previousIncome) / $previousIncome;
+                                                            $growthRates[] = $growthRate;
+                                                            $totalGrowth += $growthRate;
+                                                            $monthsCount++;
+                                                        }
+
+                                                        $previousIncome = $row['total_income'];
+                                                    }
+                                                }
+
+                                                // Calculate the average growth rate (if data exists)
+                                                $averageGrowthRate = ($monthsCount > 0) ? $totalGrowth / $monthsCount : 0;
+
+                                                // Predict income for the next 6 months based on the last month's income and average growth rate
+                                                $lastMonthIncome = $previousIncome;
+                                                $predictedIncomes = [];
+                                                $totalPredictedIncome = 0;  // Variable to store the total predicted income
+
+                                                for ($i = 1; $i <= 6; $i++) {
+                                                    $predictedMonth = date("Y-m", strtotime("+$i months", strtotime($currentMonth)));
+                                                    $nextMonthIncome = $lastMonthIncome * (1 + $averageGrowthRate);  // Apply growth rate to last month’s income
+                                                    $predictedIncomes[] = [
+                                                        'month' => $predictedMonth,
+                                                        'predicted_income' => $nextMonthIncome
+                                                    ];
+                                                    $totalPredictedIncome += $nextMonthIncome;  // Add to total predicted income
+                                                    $lastMonthIncome = $nextMonthIncome;  // Update for the next iteration
+                                                }
+
+                                                // Close connection
+                                                $conn->close();
+                                                // Display the total predicted income for the next 6 months
+                                                echo number_format($totalPredictedIncome, 2);
+                                                ?>
+                                            </b></h4>
+                                        <p><b>Prediction for 6 months</b></p>
                                     </div>
                                 </div>
                                 <div class="card-footer">
                                     <div class="row">
                                         <div class="col-lg-12">
-                                            <a href="index.php?page=invoices" class="text-primary float-right">View Payments <span class="fa fa-angle-right"></span></a>
+                                            <a href="index.php?page=prediction" class="text-primary float-right">View Payments <span class="fa fa-angle-right"></span></a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    
+                    </div>
                 </div>
-            </div>      			
+            </div>
         </div>
     </div>
 </div>
 <script>
-	$('#manage-records').submit(function(e){
+    $('#manage-records').submit(function(e) {
         e.preventDefault()
         start_load()
         $.ajax({
-            url:'ajax.php?action=save_track',
+            url: 'ajax.php?action=save_track',
             data: new FormData($(this)[0]),
             cache: false,
             contentType: false,
             processData: false,
             method: 'POST',
             type: 'POST',
-            success:function(resp){
-                resp=JSON.parse(resp)
-                if(resp.status==1){
-                    alert_toast("Data successfully saved",'success')
-                    setTimeout(function(){
+            success: function(resp) {
+                resp = JSON.parse(resp)
+                if (resp.status == 1) {
+                    alert_toast("Data successfully saved", 'success')
+                    setTimeout(function() {
                         location.reload()
-                    },800)
+                    }, 800)
 
                 }
-                
+
             }
         })
     })
-    $('#tracking_id').on('keypress',function(e){
-        if(e.which == 13){
+    $('#tracking_id').on('keypress', function(e) {
+        if (e.which == 13) {
             get_person()
         }
     })
-    $('#check').on('click',function(e){
-            get_person()
+    $('#check').on('click', function(e) {
+        get_person()
     })
-    function get_person(){
-            start_load()
-        $.ajax({
-                url:'ajax.php?action=get_pdetails',
-                method:"POST",
-                data:{tracking_id : $('#tracking_id').val()},
-                success:function(resp){
-                    if(resp){
-                        resp = JSON.parse(resp)
-                        if(resp.status == 1){
-                            $('#name').html(resp.name)
-                            $('#address').html(resp.address)
-                            $('[name="person_id"]').val(resp.id)
-                            $('#details').show()
-                            end_load()
 
-                        }else if(resp.status == 2){
-                            alert_toast("Unknow tracking id.",'danger');
-                            end_load();
-                        }
+    function get_person() {
+        start_load()
+        $.ajax({
+            url: 'ajax.php?action=get_pdetails',
+            method: "POST",
+            data: {
+                tracking_id: $('#tracking_id').val()
+            },
+            success: function(resp) {
+                if (resp) {
+                    resp = JSON.parse(resp)
+                    if (resp.status == 1) {
+                        $('#name').html(resp.name)
+                        $('#address').html(resp.address)
+                        $('[name="person_id"]').val(resp.id)
+                        $('#details').show()
+                        end_load()
+
+                    } else if (resp.status == 2) {
+                        alert_toast("Unknow tracking id.", 'danger');
+                        end_load();
                     }
                 }
-            })
+            }
+        })
     }
 </script>
---
+++ prediction.php
@@ -0,0 +1,165 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";  // Change to your DB username
+$password = "";      // Change to your DB password
+$dbname = "house_rental_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get current date and calculate the last 6 months
+$currentMonth = date("Y-m-01 00:00:00");
+$sixMonthsAgo = date("Y-m-d H:i:s", strtotime("-6 months", strtotime($currentMonth)));
+
+// Query to get payment data from the last 6 months
+$sql = "SELECT DATE_FORMAT(date_created, '%Y-%m') AS payment_month, SUM(amount) AS total_income
+        FROM payments
+        WHERE date_created >= '$sixMonthsAgo'
+        GROUP BY payment_month
+        ORDER BY payment_month ASC";
+
+$result = $conn->query($sql);
+
+$monthsData = [];
+$previousIncome = null;
+$growthRates = [];
+$totalGrowth = 0;
+$monthsCount = 0;
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $monthsData[] = $row;
+
+        // Calculate growth rate between consecutive months
+        if ($previousIncome !== null) {
+            $growthRate = ($row['total_income'] - $previousIncome) / $previousIncome;
+            $growthRates[] = $growthRate;
+            $totalGrowth += $growthRate;
+            $monthsCount++;
+        }
+
+        $previousIncome = $row['total_income'];
+    }
+}
+
+// Calculate the average growth rate (if data exists)
+$averageGrowthRate = ($monthsCount > 0) ? $totalGrowth / $monthsCount : 0;
+
+// Predict income for the next 6 months based on the last month's income and average growth rate
+$lastMonthIncome = $previousIncome;
+$predictedIncomes = [];
+$totalPredictedIncome = 0; // Variable to store the total predicted income
+
+for ($i = 1; $i <= 6; $i++) {
+    $predictedMonth = date("Y-m", strtotime("+$i months", strtotime($currentMonth)));
+    $nextMonthIncome = $lastMonthIncome * (1 + $averageGrowthRate);  // Apply growth rate to last month’s income
+    $predictedIncomes[] = [
+        'month' => $predictedMonth,
+        'predicted_income' => $nextMonthIncome
+    ];
+    $totalPredictedIncome += $nextMonthIncome;  // Add to total predicted income
+    $lastMonthIncome = $nextMonthIncome;  // Update for the next iteration
+}
+
+// Close connection
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Income Prediction Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> <!-- Include Chart.js -->
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center">Income Prediction Dashboard</h1>
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Last 6 Months Income</h2>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Month</th>
+                            <th>Total Income</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($monthsData as $data): ?>
+                            <tr>
+                                <td><?php echo $data['payment_month']; ?></td>
+                                <td><?php echo "Php" . number_format($data['total_income'], 2); ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-12">
+                <h2>Predicted Income for Next 6 Months</h2>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Month</th>
+                            <th>Predicted Income</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($predictedIncomes as $predicted): ?>
+                            <tr>
+                                <td><?php echo $predicted['month']; ?></td>
+                                <td><?php echo "Php" . number_format($predicted['predicted_income'], 2); ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                        <tr>
+                            <th>Total Predicted Income</th>
+                            <th><?php echo "Php" . number_format($totalPredictedIncome, 2); ?></th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="col-md-12">
+                <h2>Income Prediction Graph</h2>
+                <canvas id="incomePredictionChart"></canvas> <!-- Canvas for the chart -->
+            </div>
+        </div>
+    </div>
+
+    <script>
+        // Prepare the data for the chart
+        var ctx = document.getElementById('incomePredictionChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'line', // Type of chart: line
+            data: {
+                labels: <?php echo json_encode(array_column($predictedIncomes, 'month')); ?>, // Months (X-axis)
+                datasets: [{
+                    label: 'Predicted Income',
+                    data: <?php echo json_encode(array_column($predictedIncomes, 'predicted_income')); ?>, // Income (Y-axis)
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)', // Light blue background
+                    borderColor: 'rgba(75, 192, 192, 1)', // Blue line
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
+</body>
+
+</html>
