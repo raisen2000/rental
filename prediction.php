989c257f441a993da73fca45f6d2fcf034e09967
Grayyy1219 <lance.musngi@gmail.com>
2024-10-26 17:41:39 +0800

    d

--- prediction.php
+++ prediction.php
@@ -1,14 +1,6 @@
 <?php
-// Database connection
-$servername = "localhost";
-$username = "root";  // Change to your DB username
-$password = "";      // Change to your DB password
-$dbname = "house_rental_db";
+include 'db_connect.php';
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
