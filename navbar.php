d3f34dfb04cdb9c006f974ec3acdedb8c58f22a6
raisen2000 <guevarraraisen9@gmail.com>
2024-10-26 17:43:56 +0800

    ddd

--- navbar.php
+++ navbar.php
@@ -11,7 +11,7 @@
 <nav id="sidebar" class='mx-lt-5 bg-dark'>
 
 	<div class="sidebar-list">
-		<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-tachometer-alt "></i></span> Dashboard</a>
+		<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-tachometer-alt "></i></span> Dashboardsssss</a>
 		<a href="index.php?page=categories" class="nav-item nav-categories"><span class='icon-field'><i class="fa fa-th-list "></i></span> House Type</a>
 		<a href="index.php?page=houses" class="nav-item nav-houses"><span class='icon-field'><i class="fa fa-home "></i></span> Houses</a>
 		<a href="index.php?page=tenants" class="nav-item nav-tenants"><span class='icon-field'><i class="fa fa-user-friends "></i></span> Tenants</a>
