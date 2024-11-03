e811820e858000d121c837377adbe24cc1c42702
Grayyy1219 <lance.musngi@gmail.com>
2024-11-03 10:04:31 +0800

    dsda

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,13 @@
 <?php
+if (is_live_environment()) {
+    $conn = new mysqli('localhost', 'u297599468_ohrmslpa', 'Ohrmslpa2024', 'u297599468_ohrmslpa_db') or die("Could not connect to mysql" . mysqli_error($con));
+    echo "<script>console.log('live');</script>";
+} else {
+    $conn = new mysqli('localhost', 'root', '', 'house_rental_db') or die("Could not connect to mysql" . mysqli_error($con));
+    echo "<script>console.log('local');</script>";
+}
 
-// // local
-// $conn = new mysqli('localhost', 'root', '', 'house_rental_db') or die("Could not connect to mysql" . mysqli_error($con));
-
-// live
-$conn= new mysqli('localhost','u297599468_ohrmslpa','Ohrmslpa2024','u297599468_ohrmslpa_db')or die("Could not connect to mysql".mysqli_error($con));
+function is_live_environment()
+{
+    return $_SERVER['SERVER_NAME'] === 'ohrmslpa.site';
+}
