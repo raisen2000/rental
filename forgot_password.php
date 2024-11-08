e933c5add705b97d337eadc01b53422cf607bc19
Grayyy1219 <lance.musngi@gmail.com>
2024-11-08 13:37:38 +0800

    d

--- forgot_password.php
+++ forgot_password.php
@@ -7,7 +7,7 @@ include 'db_connect.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;d
+use PHPMailer\PHPMailer\Exception;
 
 require 'assets/vendor/phpmailer/phpmailer/src/Exception.php';
 require 'assets/vendor/phpmailer/phpmailer/src/PHPMailer.php';
