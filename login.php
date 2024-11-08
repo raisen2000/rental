d7b4174916453dfd149a3a99f9c5f20e9e094937
Grayyy1219 <lance.musngi@gmail.com>
2024-11-08 13:00:04 +0800

    d

--- login.php
+++ login.php
@@ -16,7 +16,7 @@ ob_end_flush();
 <head>
 	<meta charset="utf-8">
 	<meta content="width=device-width, initial-scale=1.0" name="viewport">
-
+	<link rel="shortcut icon" href="logo.ico" type="image/x-icon">
 	<title><?php echo $_SESSION['system']['name'] ?></title>
 
 
