e2a892064faca0b65bbcb5fd0d48ab5af3c5e74a
Grayyy1219 <lance.musngi@gmail.com>
2024-11-08 13:31:38 +0800

    s

--- forgot_password.php
+++ forgot_password.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include 'db_connection.php';
+include 'db_connect.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
